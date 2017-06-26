d137a7b68cb3bc106cd8877e8f5f07a016e30db6
DESKTOP-TU3L9C5\sg891 <zpgy77@gmail.com>
2017-06-27 01:30:27 +0900

    ~3.5

---
+++ Neural network.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function_0(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
+
+def step_function(x):
+   return np.array(x>0, dtype = np.int)
+
+'''x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x,y)
+plt.ylim(-0.1,1.1)
+plt.show()
+'''
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+'''
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x,y)
+plt.ylim(-0.1,1.1)
+plt.show()
+'''
+def relu(x):
+    return np.maximun(0, x)
+
+def identity_function(x):
+    return x
+
+def init_network():
+    network = {} ###dictionary
+    network['W1'] = np.array([[0.1, 0.3, 0.5],[0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1,0.4],[0.2, 0.5],[0.3, 0.6]])
+    network['b2'] = np.array([0.1,0.2])
+    network["W3"] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+
+    return network
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x,W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2,W3) + b3
+    y = identity_function(a3)
+
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network,x)
+print(y)
+
+
